2b92112b07debe6ca1c79113c2c603a8070c93b5
Colin <colingoodale@gmail.com>
2019-06-06 18:18:30 -0500

    adjusted compression

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
+const compression = require("compression");
 const db = require("./models");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(compression());
 app.use(express.static(path.join(__dirname, './public')));
 
 // Routes
