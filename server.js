520426dd6ec2e3fdfb496309363f9ce05b270c05
Colin <colingoodale@gmail.com>
2019-08-21 13:12:20 -0500

    -?

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const compression = require("compression");
 const db = require("./models");
 
 const app = express();
-const PORT = (process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(compression());
