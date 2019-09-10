06310b2192fbf80eded3f0950524de3ea0e692f3
Colin <colingoodale@gmail.com>
2019-09-10 08:40:50 -0500

    texas study get route corrected

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,5 +5,8 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "/index"));
     });
+    app.get("/texas-study", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/texas_study.html"));
+    })
 
 };
